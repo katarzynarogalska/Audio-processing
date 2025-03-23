48987a3c1feff7cada2f9b2874a70148a6dc5655
Kasia Rogalska <kasiarogal03@gmail.com>
2025-03-23 17:09:28 +0100

    'extras'

--- app_code.py
+++ app_code.py
@@ -15,10 +15,17 @@ st.title('Audio analysis app')
 with st.sidebar:
         st.subheader('File upload')
         uploaded_file = st.file_uploader("Choose audio file to upload", type=["wav"])
-      
-        st.subheader("Analysis Options")
-        frame_check = st.checkbox('Show frame-level analysis')
-        clip_check = st.checkbox('Show clip-level analysis')
+        if uploaded_file is not None:
+            y,sr = librosa.load(uploaded_file, sr=None)
+            st.markdown('Uploaded file classified as:')
+            st.write(speech_music(y,sr))
+            classif = speech_music(y,sr)
+            if classif=='Music':
+                bpm = beats(y,sr)
+                st.write(bpm)
+            st.subheader("Analysis Options")
+            frame_check = st.checkbox('Show frame-level analysis')
+            clip_check = st.checkbox('Show clip-level analysis')
         
 with st.container():
     if uploaded_file is None:
@@ -35,94 +42,96 @@ with st.container():
 with st.container(border=True):
     if uploaded_file is not None:
         st.header('Audio timeline') 
-        y,sr = librosa.load(uploaded_file, sr=None)
+       
         duration = librosa.get_duration(y=y, sr=sr)
-        frame_length =0.05*duration
+        frame_length =0.01*duration
         plot_audio(y,sr)
 
 
+if uploaded_file is not None:
+    if frame_check:
+        st.header('Audio frame characteristics')
+        with st.container(border=True):
+            if uploaded_file is not None:
+                with st.container(border=True):
+                    st.subheader('Volume analysis')
+                    loudness(y,sr, frame_length)
+                with st.container(border=True):
+                    st.subheader('Short Time Energy analysis')
+                    short_time_energy(y,sr, frame_length)
+                with st.container(border=True):
+                    st.subheader('Zero Crossing Rate analysis')
+                    zero_crossing_rate(y,sr, frame_length)
+                with st.container():
+                    st.subheader('Silence detection')
+                    volume = loudness(y,sr,frame_length, plot=False)
+                    min_volume = np.min(volume)
+                    max_volume = np.max(volume)
+                    mid = (max_volume - min_volume)/2
 
-if frame_check:
-    st.header('Audio frame characteristics')
-    with st.container(border=True):
-        if uploaded_file is not None:
-            with st.container(border=True):
-                st.subheader('Volume analysis')
-                loudness(y,sr, frame_length)
-            with st.container(border=True):
-                st.subheader('Short Time Energy analysis')
-                short_time_energy(y,sr, frame_length)
-            with st.container(border=True):
-                st.subheader('Zero Crossing Rate analysis')
-                zero_crossing_rate(y,sr, frame_length)
-            with st.container():
-                st.subheader('Silence detection')
-                volume = loudness(y,sr,frame_length, plot=False)
-                min_volume = np.min(volume)
-                max_volume = np.max(volume)
-                mid = (max_volume - min_volume)/2
+                    zcr = zero_crossing_rate(y,sr, frame_length, plot=False)
+                    min_zcr = np.min(zcr)
+                    max_zcr = np.max(zcr)
+                    mid_zcr = (max_zcr-min_zcr)/2
 
-                zcr = zero_crossing_rate(y,sr, frame_length, plot=False)
-                min_zcr = np.min(zcr)
-                max_zcr = np.max(zcr)
-                mid_zcr = (max_zcr-min_zcr)/2
+                    
+                    vol_slider = st.slider('Choose volume threshold', min_value=float(min_volume),max_value = float(max_volume), value=float(mid))
+                    zcr_slider = st.slider('Choose ZCR threshold', min_value=float(min_zcr), max_value=float(max_zcr), value = float(mid_zcr))
+                    #ste_slider = st.slider('Choose STE threshold', min_value=float(min_ste), max_value=float(max_ste), value=float(mid_ste), step=1e-6)
+                    classify_silence(y,sr,frame_length=frame_length, vol_t=vol_slider, zcr_t=zcr_slider)
+                with st.container():
+                    st.subheader('Fundamental Frequency')
+                    f0(y,sr)
 
-                
-                vol_slider = st.slider('Choose volume threshold', min_value=float(min_volume),max_value = float(max_volume), value=float(mid))
-                zcr_slider = st.slider('Choose ZCR threshold', min_value=float(min_zcr), max_value=float(max_zcr), value = float(mid_zcr))
-                #ste_slider = st.slider('Choose STE threshold', min_value=float(min_ste), max_value=float(max_ste), value=float(mid_ste), step=1e-6)
-                classify_silence(y,sr,frame_length=frame_length, vol_t=vol_slider, zcr_t=zcr_slider)
-            with st.container():
-                st.subheader('Fundamental Frequency')
-                f0(y,sr)
+    # clip based analysis
+    if clip_check:
+        st.header('Clip based analysis')
+        st.write('Volume related metrics')
+        with st.container():
+            v1 = vstd(y,sr)
+            vdr = volume_dynamic_range(y,sr)
+            vu = volume_undulation(y,sr)
+            df =pd.DataFrame({
+                "Metric" : ['VSTD', 'VDR', 'VU'],
+                "Value":[v1,vdr, vu]
+            })
+            df = df.reset_index(drop=True)
+            st.table(df)
+            if st.button('Save Volume Metrics', key='save_volume_metrics'):
+                file_path = os.path.join('report', 'volume_metrics.csv')
+                df.to_csv(file_path, index=False)
+                st.success(f"Table saved as {file_path}")
 
-# clip based analysis
-if clip_check:
-    st.header('Clip based analysis')
-    st.write('Volume related metrics')
-    with st.container():
-        v1 = vstd(y,sr)
-        vdr = volume_dynamic_range(y,sr)
-        vu = volume_undulation(y,sr)
-        df =pd.DataFrame({
-            "Metric" : ['VSTD', 'VDR', 'VU'],
-            "Value":[v1,vdr, vu]
-        })
-        df = df.reset_index(drop=True)
-        st.table(df)
-        if st.button('Save Volume Metrics', key='save_volume_metrics'):
-            file_path = os.path.join('report', 'volume_metrics.csv')
-            df.to_csv(file_path, index=False)
-            st.success(f"Table saved as {file_path}")
+        st.write('Energy related metrics')
+        with st.container():
+            l = lster(y,sr)
+            entropy = energy_entropy(y,sr)
+            df =pd.DataFrame({
+                "Metric" : ['LSTER', 'ENTROPY'],
+                "Value":[l,entropy]
+            })
+            df = df.reset_index(drop=True)
+            st.table(df)
+            if st.button('Save Energy Metrics', key='save_energy_metrics'):
+                file_path = os.path.join('report', 'energy_metrics.csv')
+                df.to_csv(file_path, index=False)
+                st.success(f"Table saved as {file_path}")
 
-    st.write('Energy related metrics')
-    with st.container():
-        l = lster(y,sr)
-        entropy = energy_entropy(y,sr)
-        df =pd.DataFrame({
-            "Metric" : ['LSTER', 'ENTROPY'],
-            "Value":[l,entropy]
-        })
-        df = df.reset_index(drop=True)
-        st.table(df)
-        if st.button('Save Energy Metrics', key='save_energy_metrics'):
-            file_path = os.path.join('report', 'energy_metrics.csv')
-            df.to_csv(file_path, index=False)
-            st.success(f"Table saved as {file_path}")
-
-    st.write('Zero Crossing Rate related metrics')
-    with st.container():
-        hzcr = hzcrr(y,sr)
-        df =pd.DataFrame({
-            "Metric" : ['HZCRR'],
-            "Value":[hzcr]
-        })
-        df = df.reset_index(drop=True)
-        st.table(df)
-        if st.button('Save ZCR Metrics', key='save_zcr_metrics'):
-            file_path = os.path.join('report', 'zcr_metrics.csv')
-            df.to_csv(file_path, index=False)
-            st.success(f"Table saved as {file_path}")
+        st.write('Zero Crossing Rate related metrics')
+        with st.container():
+            hzcr = hzcrr(y,sr)
+            df =pd.DataFrame({
+                "Metric" : ['HZCRR'],
+                "Value":[hzcr]
+            })
+            df = df.reset_index(drop=True)
+            st.table(df)
+            if st.button('Save ZCR Metrics', key='save_zcr_metrics'):
+                file_path = os.path.join('report', 'zcr_metrics.csv')
+                df.to_csv(file_path, index=False)
+                st.success(f"Table saved as {file_path}")
+        st.subheader('Extra audio features')
+        spectral_centroid(y,sr,frame_length)
 
 
   
--- clip_algorithms.py
+++ clip_algorithms.py
@@ -8,6 +8,7 @@ import math
 import plotly.graph_objects as go
 from scipy.signal import find_peaks
 
+# Volume based ----------------------------------------------------------------------------------------
 def vstd(y,sr,frame_length=1):
     volumes = loudness(y,sr,frame_length,plot=False)
     std = np.std(volumes)
@@ -31,7 +32,7 @@ def volume_undulation(y,sr,frame_length=1):
     return vu
 
 
-# Energy based
+# Energy based --------------------------------------------------------------------------------------------------------
 
 def lster(y,sr,frame_length=1):
     energies = short_time_energy(y,sr,frame_length, plot=False)
@@ -47,15 +48,11 @@ def energy_entropy(y,sr,frame_length=1):
     frame_size = int(frame_length * sr) #ilość próbek w ramce
     K = 441
     normalized_ste =[] 
-    #dla każdej ramki
-
     for i in range(len(ste)):
         frame_start = i * frame_size
         frame_end = (i + 1) * frame_size
         frame = y[frame_start:frame_end]
         total_energy = ste[i]
-
-        #przejscie po probkach w danej ramce i podzial na segmenty
         for start in range(0, len(frame), K):
             segment_end = min(start+K, len(ste))
             segment = frame[start:segment_end]
@@ -66,13 +63,12 @@ def energy_entropy(y,sr,frame_length=1):
             else:
                 normalized_energy = 0
             normalized_ste.append(normalized_energy)
-    
     #clipping to avoid log(0)
     normalized_ste = np.clip(normalized_ste, 1e-10, 1)
     entropy = -np.sum(normalized_ste * np.log(normalized_ste))
     return entropy
 
-# ZCR Based
+# ZCR Based ---------------------------------------------------------------------------------------------------------
 def hzcrr(y,sr,frame_length=1):
     zcrs = zero_crossing_rate(y,sr,frame_length, plot=False)
     avgzcr = np.average(zcrs)
@@ -82,21 +78,56 @@ def hzcrr(y,sr,frame_length=1):
     return sum/(2*len(zcrs))
 
 
-# music speech detection
+# music speech detection ------------------------------------------------------------------------------------------------
 def speech_music(y,sr):
     l = lster(y,sr)
     print(l)
-    if l>0.17 and l<0.5:
+    if l>0.2 and l<0.5:
         return 'Speech'
-    elif l<0.17:
+    elif l<0.14:
         return 'Music'
+    else:
+        return 'Speech or Music'
+    
 
+# Extras --------------------------------------------------------------------------------------------------------------
 
+def spectral_centroid(y,sr,frame_length):
+    frame_size = int(frame_length*sr)
+    sc = librosa.feature.spectral_centroid(y=y, sr=sr, n_fft=frame_size)[0]
+    band = librosa.feature.spectral_bandwidth(y=y, sr=sr, n_fft=frame_size)[0]
+    times = librosa.frames_to_time(range(len(sc)), sr=sr)
+    # interactive_plot(x=times, y=sc,title='Spectral Centroid plot', x_axis='Time [s]', y_axis='Spectral Centroid', text='', key='spectral_centroid')
+    # return sc
 
+    fig, ax = plt.subplots(figsize=(10, 5))
+    ax.plot(times, sc, label="Spectral Centroid", color='#0d0469', linewidth=2)
+    ax.fill_between(times, sc - band, sc + band, 
+                    color='#a0c1db', alpha=0.3, label="Spectral Bandwidth")
+
+    ax.set_xlabel("Time [s]")
+    ax.set_ylabel("Frequency (Hz)")
+    ax.set_title("Spectral Centroid & Bandwidth Over Time")
+    ax.legend()
+
+    # Display the plot in Streamlit
+    st.pyplot(fig)
+
+#only for clips classified as music
+def beats(y,sr):
+    tempo= librosa.feature.tempo(y=y, sr=sr)
+    if tempo<86:
+        return f"Low BPM value : {tempo} - slow music"
+    elif tempo<100:
+        return f"Medium BPM value : {tempo} - moderate pace music"
+    elif tempo<140:
+        return f"High BPM value : {tempo} - fast music"
+    else:
+        return f"Very high BPM value : {tempo} - very fast music"
 
 
 if __name__ == '__main__':
-    audio = 'sample_data/zdanie_1.wav'
+    audio = 'sample_data/m2.wav'
     y,sr = librosa.load(audio, sr=None)
 
-    print(speech_music(y,sr))
+    print(beats(y,sr))
